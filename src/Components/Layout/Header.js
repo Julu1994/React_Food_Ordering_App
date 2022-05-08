a0161ea9c80d4c9a98ac8de230f04d5b434ce13d
julu1994 <mrjewel837@gmail.com>
2022-05-08 02:12:32 +0200

    fetching data

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from "react";
-import foodImg from "../../Images/headerImg.jpeg";
+import foodImg from "../../Images/drinks3.jpeg";
 import CartButton from "../SubComponents/CartButton";
 import "./Header.css";
+import Drinks from "./Drinks";
 
 function Header() {
     return (
         <Fragment>
             <header className="header">
                 <h1 className="logo">
-                    <span className="logo-title">FOODMART</span>
-                    <span className="motto">STAY AT HOME</span>
+                    <span className="logo-title">DRUNK BAR</span>
+                    <span className="motto">I am so drunk</span>
                 </h1>
                 <CartButton />
             </header>
@@ -17,6 +18,7 @@ function Header() {
             <div className="HeaderImage">
                 <img src={foodImg} alt="Cover" />
             </div>
+            <Drinks />
         </Fragment>
     );
 }
