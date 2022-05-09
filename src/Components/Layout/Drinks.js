73e64a1ecd7876b8f0d408730558d4c28bbe1544
julu1994 <mrjewel837@gmail.com>
2022-05-09 03:33:54 +0200

    useEffect bugs

--- src/Components/Layout/Drinks.js
+++ src/Components/Layout/Drinks.js
@@ -2,25 +2,23 @@ import React, { useState, useEffect } from "react";
 
 function Drinks() {
     const [users, setUsers] = useState();
-    const getApiData = async () => {
-        const response = await fetch("https://fakestoreapi.com/products/").then(
-            (response) => response.json()
-        );
-
-        // update the state
-        setUsers(response);
-    };
+    const [test, setTest] = useState("Old");
+    // const getApiData = async () => {
+    //     const response = await fetch("https://fakestoreapi.com/products/").then(
+    //         (response) => response.json()
+    // );
     useEffect(() => {
-        getApiData();
-    }, []);
+        fetch("https://fakestoreapi.com/products/")
+            .then((response) => response.json())
+            .then((json) => setUsers(json))
+            .then((json) => console.log(json));
+    }, [setTest]);
 
     return (
         <div className="app">
-            {users?.map((user) => (
-                <div className="item-container">
-                    Id:{user.id} <div className="title">Title:{user.title}</div>
-                </div>
-            ))}
+            {users?.map((user) => {
+                return <h1>{JSON.stringify(user)}</h1>;
+            })}
             {/* {users &&
                 users.map((user) => (
                     <div className="item-container">
@@ -28,6 +26,8 @@ function Drinks() {
                         <div className="title">Title:{user.title}</div>
                     </div>
                 ))} */}
+            <button onClick={() => setTest("New")}>click </button>
+            <h1>{test}</h1>
         </div>
     );
 }
