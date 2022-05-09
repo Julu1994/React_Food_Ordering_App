b6fb74459b50f95cff92b1dc6b772ad8f2d32c4f
julu1994 <mrjewel837@gmail.com>
2022-05-09 21:56:02 +0200

    api bugs and useEffect

--- src/Components/Layout/Drinks.js
+++ src/Components/Layout/Drinks.js
@@ -1,35 +1,14 @@
 import React, { useState, useEffect } from "react";
+import Card from "../SubComponents/Card";
 
 function Drinks() {
-    const [users, setUsers] = useState();
-    const [test, setTest] = useState("Old");
-    // const getApiData = async () => {
-    //     const response = await fetch("https://fakestoreapi.com/products/").then(
-    //         (response) => response.json()
-    // );
-    useEffect(() => {
-        fetch("https://fakestoreapi.com/products/")
-            .then((response) => response.json())
-            .then((json) => setUsers(json))
-            .then((json) => console.log(json));
-    }, [setTest]);
+    const [drinksItem, setDrinksItem] = useState();
 
-    return (
-        <div className="app">
-            {users?.map((user) => {
-                return <h1>{JSON.stringify(user)}</h1>;
-            })}
-            {/* {users &&
-                users.map((user) => (
-                    <div className="item-container">
-                        Id:{user.id}{" "}
-                        <div className="title">Title:{user.title}</div>
-                    </div>
-                ))} */}
-            <button onClick={() => setTest("New")}>click </button>
-            <h1>{test}</h1>
-        </div>
+    fetch("https://fakestoreapi.com/products/1").then((response) =>
+        console.log(response)
     );
+
+    return <div className="app"></div>;
 }
 
 export default Drinks;
--- src/Components/SubComponents/Card.js
+++ src/Components/SubComponents/Card.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./Card.css";
 
-function Card({ drinksInfo }) {
+function Card(drinks) {
     return (
         <div className="main-div">
-            {drinksInfo.map((drinks) => (
-                <div className="card">
-                    <img className="card-img" src={drinks.image} alt="" />
-                    <h1 className="card-tittle">{drinks.title}</h1>
-                    <p className="card-description">{drinks.price}</p>
-                    <button className="card-button">Buy</button>
-                </div>
-            ))}
+            <div className="card">
+                <img className="card-img" src={drinks.image} alt="" />
+                <h1 className="card-tittle">{drinks.title}</h1>
+                <p className="card-description">{drinks.price}</p>
+                <button className="card-button">Buy</button>
+            </div>
         </div>
     );
 }
