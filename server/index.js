fe6f0958ccfcb259d90f7fffd4252e43e7b95341
julu1994 <mrjewel837@gmail.com>
2022-05-11 01:04:29 +0200

    server building

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+app.listen(5000, () => console.log("Server is up and running"));
+
+app.use("/", require("./Router/router"));
