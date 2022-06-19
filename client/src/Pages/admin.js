8543827a078ce36a1dce812194b270740d62badf
julu1994 <mrjewel837@gmail.com>
2022-06-20 00:10:26 +0200

    building admin page

--- client/src/Pages/admin.js
+++ client/src/Pages/admin.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Admin() {
-    return <div>Admin</div>;
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        showProducts();
+    }, []);
+    async function showProducts() {
+        const getProducts = await Axios.get("http://localhost:5000/admin");
+        console.log(getProducts);
+    }
+    return <div></div>;
 }
 
 export default Admin;
