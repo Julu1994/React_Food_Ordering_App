94c6438a25d8635ea087ac0bae0c4e095298382e
julu1994 <mrjewel837@gmail.com>
2022-06-20 00:41:00 +0200

    adding cors in server to allow frontend for data

--- server/index.js
+++ server/index.js
@@ -2,21 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 app.listen(5000, () => console.log("Server is up and running"));
 
-
 app.use("/admin", require("./Router/router"));
 
-mongoose.connect(
-    process.env.MONGODB_SECRET_URL,
-    (error) => {
-        if (error) return console.error(error);
-        console.log("Successfully conncected to mongoDB");
-    }
-);
+mongoose.connect(process.env.MONGODB_SECRET_URL, (error) => {
+    if (error) return console.error(error);
+    console.log("Successfully conncected to mongoDB");
+});
 
-app.use("/", require("./Router/router"));  
+app.use("/", require("./Router/router"));
